04d6aee86976db34517a96de7f2aade51e6df399
TxellVilamajo <txell.vilamajo@estudiantat.upc.edu>
2025-03-12 09:57:08 +0100

    ex2.cpp

--- ex2.cpp
+++ ex2.cpp
@@ -1,26 +1,24 @@
 #include <Arduino.h>
 
-#define LED_BUILTIN 2      // Definir el pin del LED
-#define DELAY_TIME 1000   // Tiempo de espera de 1000 ms
+#define LED_BUILTIN 2      // Definim el pin del LED
+#define DELAY_TIME 1000   // Definim un temps d'epera de 1000 ms = 1s
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);  // Configurar el pin como salida
-  Serial.begin(115200);          // Inicializar la comunicación serial
+  pinMode(LED_BUILTIN, OUTPUT);  // Configurem el pin del LED com a pin de sortida
+  Serial.begin(115200);          // Iniciem la comunicació amb el port sèrie
 }
 
 void loop() {
   // Encender el LED
   digitalWrite(LED_BUILTIN, HIGH);  
-  Serial.println("ON");            // Enviar "ON" por el puerto serial
-  delay(DELAY_TIME);               // Esperar 1000 ms
+  Serial.println("ON");            // Enviem "ON" pel port serial
+  delay(DELAY_TIME);               // Esperem 1000 ms (temps definit a l'inici del programa)
 
   // Apagar el LED
   digitalWrite(LED_BUILTIN, LOW);  
-  Serial.println("OFF");           // Enviar "OFF" por el puerto serial
-  delay(DELAY_TIME);               // Esperar 1000 ms
+  Serial.println("OFF");           // Enviem "OFF" pel port serial
+  delay(DELAY_TIME);               // Esperem 1000 ms (temps definit a l'inici del programa)
 }
-
-
 
 
 
